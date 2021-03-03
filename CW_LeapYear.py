5b6020aeb98668027ac65a2392b4f5fa24bfcd98
Christopher Weiner <chriscweiner@gmail.com>
2021-03-03 10:46:31 -0800

    Implemented LeapYear with addition of %100, {test worked for one, broke for another, will need to do another round of test and code}

--- CW_LeapYear.py
+++ CW_LeapYear.py
@@ -5,8 +5,12 @@
 def LeapYear(x):
     ret = ""
     if(x % 4) == 0:
-        ret = (str(x) + " is a leap year")
-        return ret
+        if(x % 100) == 0:
+            ret = (str(x) + " is not a leap year")
+            return ret
+        else:
+            ret = (str(x) + " is a leap year")
+            return ret  
     else:
         ret = (str(x) + " is not a leap year")
         return ret
