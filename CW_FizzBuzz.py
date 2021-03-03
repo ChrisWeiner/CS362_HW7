24c66e3701cc4600fc0cc44a282fe11dcf10aaea
Christopher Weiner <chriscweiner@gmail.com>
2021-03-03 08:43:22 -0800

    Implemented FizzBuzz method {built a string and used a comparison flag to see if the value needed to be added, or if we can skip it and just add in a whitespace character while ensuring the rules of FizzBuzz were maintained.}
    Implemented FizzBuzz method {built a string and used a comparison flag to see if the value needed to be added, or if we can skip it and just add in a whitespace character while ensuring the rules of FizzBuzz were maintained.}

--- CW_FizzBuzz.py
+++ CW_FizzBuzz.py
@@ -3,4 +3,18 @@
 #FizzBuzz
 
 def FizzBuzz():
-    pass
+    ret = ""
+    check = 0
+    for val in range(1,101):
+        if val % 3 == 0:
+            ret += "Fizz"
+            check = 1
+        if val % 5 == 0:
+            ret += "Buzz"
+            check = 1
+        if check == 0:
+            ret += str(val)
+        ret += " "
+        check = 0
+    return ret
+#FizzBuzz()
