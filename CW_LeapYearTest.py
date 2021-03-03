cb88fa2bad5e5d1e50272d9c77ff28daff73df0d
Christopher Weiner <chriscweiner@gmail.com>
2021-03-03 10:43:52 -0800

    Wrote second test, {tested if 1700 was a leap year, test failed}

--- CW_LeapYearTest.py
+++ CW_LeapYearTest.py
@@ -7,6 +7,8 @@ import unittest
 class TestCase1(unittest.TestCase):
     def test1(self):
         self.assertEqual(LP.LeapYear(2000),"2000 is a leap year")
+    def test2(self):
+        self.assertEqual(LP.LeapYear(1700),"1700 is not a leap year")
 
 if __name__ == "__main__":
     unittest.main()
